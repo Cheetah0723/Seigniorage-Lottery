0357c7307fa0ff5dc32348ea0e5fef9e53fffa68
Cheetah0723 <iyuki723@gmail.com>
2022-04-24 05:19:57 +0900

    space

--- src/components/peglottery.jsx
+++ src/components/peglottery.jsx
@@ -12,7 +12,7 @@ export const PegLottery = (props) => {
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                  <img src={d.emp} className="emp" alt="emp"></img>
+                  <img src={d.emp} className="emp" alt="emp" />
                   <div className="content-border">
                     <h3>{d.title}</h3>
                   </div>
